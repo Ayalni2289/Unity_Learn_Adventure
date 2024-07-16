4f89aad501f5fde6590df97aa4acf246cdc918f0
Ayalni2289 <alininspor@hotmail.com>
2024-07-16 15:05:36 +0300

    player movement updated with arrow keys.

--- AdventureGame/Assets/Scripts/PlayerController.cs
+++ AdventureGame/Assets/Scripts/PlayerController.cs
@@ -6,41 +6,21 @@ using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-{
-    float horizontal = 0.0f;
-    float vertical = 0.0f;
-    if (Keyboard.current.leftArrowKey.isPressed)
-    {
-        horizontal = -.10f;
- 	    }
-    else if (Keyboard.current.rightArrowKey.isPressed)
-    {
-        horizontal = .10f;
-    }
-    if(Keyboard.current.upArrowKey.isPressed)
-    {
-        vertical = .10f;
-    }
-    else if(Keyboard.current.downArrowKey.isPressed)
-    {
-        vertical = -.10f;
-    }
-    Debug.Log(horizontal);
-    Debug.Log(vertical);
+    public InputAction MoveAction;
 
 
-    Vector2 position = transform.position;
-    position.y = position.y + 0.1f * vertical;
-    position.x = position.x + 0.1f * horizontal;
-    transform.position = position;
-}
+   // Start is called before the first frame update
+   void Start()
+   {
+      MoveAction.Enable();  
+   }
 
+   // Update is called once per frame
+   void Update()
+   {
+     Vector2 move = MoveAction.ReadValue<Vector2>();
+     Debug.Log(move);
+     Vector2 position = (Vector2)transform.position + move * 0.1f;
+     transform.position = position;
+   }
 }
