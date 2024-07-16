55c0371852cc987e011fa7c62a19be7f1cfdc193
Ayalni2289 <alininspor@hotmail.com>
2024-07-16 14:46:07 +0300

    Player movements updated.

--- AdventureGame/Assets/Scripts/PlayerController.cs
+++ AdventureGame/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,11 +14,33 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+{
+    float horizontal = 0.0f;
+    float vertical = 0.0f;
+    if (Keyboard.current.leftArrowKey.isPressed)
     {
-        Vector2 position = transform.position;
-        position.y += 0.01f;
-        position.x += 0.01f;
-        transform.position = position;
-        
+        horizontal = -.10f;
+ 	    }
+    else if (Keyboard.current.rightArrowKey.isPressed)
+    {
+        horizontal = .10f;
     }
+    if(Keyboard.current.upArrowKey.isPressed)
+    {
+        vertical = .10f;
+    }
+    else if(Keyboard.current.downArrowKey.isPressed)
+    {
+        vertical = -.10f;
+    }
+    Debug.Log(horizontal);
+    Debug.Log(vertical);
+
+
+    Vector2 position = transform.position;
+    position.y = position.y + 0.1f * vertical;
+    position.x = position.x + 0.1f * horizontal;
+    transform.position = position;
+}
+
 }
